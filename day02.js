05690abe392245e7e068449ee34a36441f590cff
Tom Bower <me@thomasbower.net>
2020-12-02 13:55:10 +0000

    Day 2 golfing

--- day02.js
+++ day02.js
@@ -1,16 +1,14 @@
 'use strict'
 
-const parse = input => {
-  const inputs = input.split('\n');
-  return inputs.map(line => {
+const parse = input =>
+  input.split('\n').map(line => {
     const { groups } = /(?<i>\d+)-(?<j>\d+) (?<letter>\w): (?<password>\w+)/g.exec(line);
     return {
       ...groups,
       i: parseInt(groups.i),
       j: parseInt(groups.j),
     };
-  })
-}
+  });
 
 // Part 1
 // ======
@@ -28,8 +26,7 @@ const part1 = input =>
 
 const part2 = input => 
   parse(input).reduce((valid, { i, j, letter, password }) =>
-    (password[i - 1] === letter && password[j - 1] !== letter)
-      || (password[i - 1] !== letter && password[j - 1] === letter) ? valid + 1 : valid
+    ((password[i - 1] === letter) !== (password[j - 1] === letter)) ? valid + 1 : valid
   , 0);
 
 module.exports = { part1, part2 }
