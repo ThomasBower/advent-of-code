6be271a7beba4dd0d411f2b5b8a7069e37c85972
Tom Bower <me@thomasbower.net>
2020-12-07 14:10:20 +0000

    Day 5 golfing

--- solutions/day05.js
+++ solutions/day05.js
@@ -3,11 +3,9 @@
 const seatIds = input => input.split('\n').map(row =>
   parseInt(row.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2));
 
-const part1 = input => Math.max(...seatIds(input))
+const part1 = input => Math.max(...seatIds(input));
 
-const part2 = input => {
-  const ids = seatIds(input);
-  return (Math.max(...ids) + Math.min(...ids)) * (ids.length + 1) / 2 - ids.reduce((a, b) => a + b);
-};
+const part2 = input => (ids => (Math.max(...ids) + Math.min(...ids))
+  * (ids.length + 1) / 2 - ids.reduce((a, b) => a + b))(seatIds(input));
 
 module.exports = { part1, part2 }
