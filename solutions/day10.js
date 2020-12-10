8fb01ea96bd797a12cf051179d69440fe086bb29
Tom Bower <me@thomasbower.net>
2020-12-10 21:38:40 +0000

    Fix formatting in day 9

--- solutions/day10.js
+++ solutions/day10.js
@@ -7,9 +7,9 @@ const part1 = input => (([ones, _, threes]) => ones * threes)(parse(input)
     val - (arr[i - 1] || 0) === j + 1 ? cnt + 1 : cnt), [0, 0, 1]));
 
 const part2 = input => [0, ...parse(input).sort((a, b) => a - b)]
-  .reduce((combinations, val, i, arr) => [...combinations, (i === 0
-    ? 1 : arr.slice(Math.max(0, i - 3), i).map((prev, j) => val - prev <= 3
-      ? combinations[Math.max(0, i - 3) + j] : 0).reduce((a, b) => a + b))]
-  , []).pop();
+  .reduce((combinations, val, i, arr) => [...combinations, (i === 0 ?
+    1 : arr.slice(Math.max(0, i - 3), i).map((prev, j) => val - prev <= 3 ?
+      combinations[Math.max(0, i - 3) + j] : 0).reduce((a, b) => a + b))], [])
+  .pop();
 
 module.exports = { part1, part2 }
