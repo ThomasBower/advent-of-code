21957c09ed5927b018446597fd764013e57fd995
Tom Bower <me@thomasbower.net>
2020-12-10 21:15:59 +0000

    Add day 10 solution

---
+++ solutions/day10.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const parse = input => input.split('\n').map(line => Number(line));
+
+const part1 = input => (([ones, _, threes]) => ones * threes)(parse(input)
+  .sort((a, b) => a - b).reduce((diffs, val, i, arr) => diffs.map((cnt, j) =>
+    val - (arr[i - 1] || 0) === j + 1 ? cnt + 1 : cnt), [0, 0, 1]));
+
+const part2 = input => [0, ...parse(input).sort((a, b) => a - b)]
+  .reduce((p, v, i, arr) => [...p, (i === 0 ? 1 : arr.slice(Math.max(0, i - 3), i)
+    .map((prev, j) => v - prev <= 3 ? p[Math.max(0, i - 3) + j] : 0)
+    .reduce((a, b) => a + b))]
+  , []).pop();
+
+module.exports = { part1, part2 }
